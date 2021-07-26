126d6ca6e14f767c9cb26b2f36283ab62d1faef0
Daniel Collier <danielfcollier@gmail.com>
2021-07-25 23:38:19 -0300

    deployed

--- server/loadWebApp.js
+++ server/loadWebApp.js
@@ -2,6 +2,6 @@ function doGet() {
     //SpreadsheetApp.getUi();
     //DriveApp.getRootFolder();
     //UrlFetchApp.fetch("");
-    const htmlPageTemplate = HtmlService.createTemplateFromFile('index');
+    const htmlPageTemplate = HtmlService.createTemplateFromFile('main');
     return htmlPageTemplate.evaluate().setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
   }
